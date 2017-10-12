6e21978b12cb73751efcc8a72369d625412805fb
egk00 <cksk0018@naver.com>
2017-10-12 17:03:12 +0900

    행사정보 화면 이동

--- app/src/main/java/com/example/kug00/androidgithub/MainActivity.java
+++ app/src/main/java/com/example/kug00/androidgithub/MainActivity.java
@@ -3,9 +3,13 @@ package com.example.kug00.androidgithub;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutCompat;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.LinearLayout;
+
+import static android.R.attr.start;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,7 +19,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         //아이디 가져오기
-        ImageButton GoToPlan = (ImageButton) findViewById(R.id.goplan);
+        ImageButton GoToPlan = (ImageButton)findViewById(R.id.goplan);
+        LinearLayout GoParty = (LinearLayout)findViewById(R.id.goparty);
 
         //여행계획으로 화면 이동
         GoToPlan.setOnClickListener(new View.OnClickListener() {
@@ -25,5 +30,17 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(it);
             }
         });
+
+        //행사정보 화면으로 이동
+        GoParty.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent it = new Intent(MainActivity.this, PartyActivity.class);
+                startActivity(it);
+            }
+        });
+
+
+
     }
 }
