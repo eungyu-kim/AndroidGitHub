0bb1bbd59a8f3380491530135f7bb2bcb2038680
egk00 <cksk0018@naver.com>
2017-10-12 21:06:11 +0900

    메인화면과 행사화면에 현재 월 출력

--- app/src/main/java/com/example/kug00/androidgithub/MainActivity.java
+++ app/src/main/java/com/example/kug00/androidgithub/MainActivity.java
@@ -8,10 +8,19 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import static android.R.attr.start;
 
 public class MainActivity extends AppCompatActivity {
+    long mNow;
+    Date mDate;
+    //전체 일자 출력
+    SimpleDateFormat Format = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
+    SimpleDateFormat mFormat = new SimpleDateFormat("MM");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +30,8 @@ public class MainActivity extends AppCompatActivity {
         //아이디 가져오기
         ImageButton GoToPlan = (ImageButton)findViewById(R.id.goplan);
         LinearLayout GoParty = (LinearLayout)findViewById(R.id.goparty);
+        TextView Month1 = (TextView)findViewById(R.id.month1);
+        TextView Month2 = (TextView)findViewById(R.id.month2);
 
         //여행계획으로 화면 이동
         GoToPlan.setOnClickListener(new View.OnClickListener() {
@@ -40,7 +51,20 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        //현재 월 텍스트 출력
+        Month1.setText(getTime(1));
+        Month2.setText(getTime(1));
+    }
 
-
+    //파라미터에 따라 0일때 전체 일자 1일때 월 출력
+    private String getTime(int x){
+        mNow = System.currentTimeMillis();
+        mDate = new Date(mNow);
+        if (x==0)
+            return Format.format(mDate);
+        else if (x==1)
+            return mFormat.format(mDate);
+        else
+            return "현재일자를 받지 못했습니다.";
     }
 }
