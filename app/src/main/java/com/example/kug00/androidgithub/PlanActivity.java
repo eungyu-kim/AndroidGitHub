cef2975ef22f59736a2f1a86ac77df3403cef8f2
TaeHwanL <soslth@naver.com>
2017-10-12 21:39:57 +0900

    Plan 버튼 생성

--- app/src/main/java/com/example/kug00/androidgithub/PlanActivity.java
+++ app/src/main/java/com/example/kug00/androidgithub/PlanActivity.java
@@ -1,17 +1,32 @@
 package com.example.kug00.androidgithub;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageButton;
 
 /**
- * Created by kug00 on 2017-10-12.
+ * Created by soslt on 2017-10-12.
  */
 
 public class PlanActivity extends AppCompatActivity{
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.plan);
+
+        //아이디 가져오기
+        ImageButton BackHome = (ImageButton)findViewById(R.id.BackHome);
+
+        //여행계획으로 화면 이동
+        BackHome.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent it = new Intent(PlanActivity.this, MainActivity.class);
+                startActivity(it);
+            }
+        });
+
     }
 }
