47b440497007e0113ab730dde380a403b69f92a5
TaeHwanL <soslth@naver.com>
2017-10-21 00:47:31 +0900

    중분류 추가

--- app/src/main/java/com/example/kug00/androidgithub/MainActivity.java
+++ app/src/main/java/com/example/kug00/androidgithub/MainActivity.java
@@ -20,6 +20,7 @@ import java.util.Date;
 public class MainActivity extends AppCompatActivity {
     long mNow;
     Date mDate;
+
     //전체 일자 출력
     SimpleDateFormat Format = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
     SimpleDateFormat mFormat = new SimpleDateFormat("MM");
